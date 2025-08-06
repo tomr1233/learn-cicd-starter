ed885cb2a8b2a5cabe855dc8df046d5101b855d1
Tom <tom@expressnext.app>
2025-08-06 20:02:21 +1200

    fix lint error

--- main.go
+++ main.go
@@ -17,11 +17,6 @@ import (
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
-func hiimafunction(stri string) {
-	stri = "hello"
-	return stri
-}
-
 type apiConfig struct {
 	DB *database.Queries
 }
