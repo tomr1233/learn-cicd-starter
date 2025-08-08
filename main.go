20f030bc8cde3eaeb7ac21da210154d5fce41946
Tom <tom@expressnext.app>
2025-08-08 19:06:42 +1200

    fix the server error

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -93,7 +94,7 @@ func main() {
 	srv := &http.Server{
 		Addr:              ":" + port,
 		Handler:           router,
-		ReadHeaderTimeout: 5,
+		ReadHeaderTimeout: 300 * time.Second,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
